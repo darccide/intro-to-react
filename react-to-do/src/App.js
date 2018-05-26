08d56c7b9a832b36797e99e7b67b00ec1e38a0fe
Steven Thomson <steven.thomson88@gmail.com>
2018-05-26 22:30:19 +0900

    Assignment 9 completed

--- react-to-do/src/App.js
+++ react-to-do/src/App.js
@@ -33,12 +33,19 @@ class App extends Component {
      this.setState({ todos: todos });
    }
 
+   deleteTodo(index) {
+     const todos = this.state.todos.slice();
+     const todo = todos[index];
+     const newTodos = todos.filter((item) => item !== todo ); 
+     this.setState({ todos: newTodos });
+   }
+
   render() {
     return (
       <div className="App">
         <ul>
           { this.state.todos.map( (todo, index) =>
-            <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } toggleComplete={ () => this.toggleComplete(index) } />
+            <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } toggleComplete={ () => this.toggleComplete(index) } deleteTodo={ () => this.deleteTodo(index) } />
           )}
         </ul>
         <form onSubmit={ (e) => this.handleSubmit(e) }>
